eb0f56013a72fa7a776218dbe4c80b7f638e58d1
Troy Brown <44628567+TroyWasHere@users.noreply.github.com>
2023-03-17 21:52:24 +0000

    Fri, Mar 17, 2023, 2:52 PM -07:00

--- js/sounds.js
+++ js/sounds.js
@@ -4,4 +4,8 @@ sounds.forEach(sound => {
     const btn = document.createElement('button')
     btn.classList.add('btn')
     btn.innerText = sound
+
+    btn.addEventListener('click' () => {
+        document.getElementById(sound).play()
+    })
 })

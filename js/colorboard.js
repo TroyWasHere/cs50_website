99da92ccca921ee4197f0d93e41eac3940f6d574
Troy Brown <44628567+TroyWasHere@users.noreply.github.com>
2023-03-23 21:13:10 +0000

    Thu, Mar 23, 2023, 2:13 PM -07:00

--- js/colorboard.js
+++ js/colorboard.js
@@ -7,9 +7,14 @@ for (let i = 0; i <= SQUARES; i++) {
     square.classList.add('square')
     container.appendChild(square)
     square.addEventListener('mouseover', () => setColor(square))
+    square.addEventListener('mouseout', () => removeColor(square))
 }
 
 function setColor(square) {
     const color = colors[Math.floor(Math.random() * 5)]
     square.style.background = color
+}
+
+function removeColor(square) {
+    square.style.background = '#1f1f1f'
 }

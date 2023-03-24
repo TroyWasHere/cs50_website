2f2293dec1c1bf731317274bb813ea553df03124
Troy Brown <44628567+TroyWasHere@users.noreply.github.com>
2023-03-24 22:03:26 +0000

    Fri, Mar 24, 2023, 3:03 PM -07:00

--- js/typing.js
+++ js/typing.js
@@ -2,10 +2,20 @@ const textElement = document.getElementById('text')
 const speedElement = document.getElementById('speed')
 const text = "We Love Milfs"
 let idx = 1
-let speed = 300
+let speed = 300 / speedElement.value
+
+speedElement.addEventListener('input', (element) => {
+    speed = 300 / element.target.value
+})
 
 writeText()
 
 function writeText() {
-    textElement.innerText = text.slice(0,1)
+    textElement.innerText = text.slice(0,idx)
+    idx++
+    if (idx > text.length) {
+        idx = 1
+    }
+    setTimeout(writeText, speed)
 }
+

c56ba7bf29ba86d8e3e62a1dc6bad43ea4a86d22
Troy Brown <44628567+TroyWasHere@users.noreply.github.com>
2023-04-13 15:01:47 -0700

    Website Update

--- js/sounds.js
+++ js/sounds.js
@@ -5,7 +5,7 @@ sounds.forEach(sound => {
     btn.classList.add('btn')
     btn.innerText = sound
 
-    btn.addEventListener('click' () => {
+    btn.addEventListener('click', () => {
         stopSongs()
         document.getElementById(sound).play()
     })
